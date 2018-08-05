a6aa91744968397c796fde7ee9a8943c89546722
Carlyle <zachjohnsonsoftware@gmail.com>
2018-08-05 18:09:53 -0500

    anagram checker

--- arrays/anagram.py
+++ arrays/anagram.py
@@ -3,16 +3,26 @@
 def anagram(s1, s2):
     
     s1Len = s2Len = 0
+    s1Copy = s2Copy = []    
     for i in range(len(s1)):
         if not s1[i].isspace():
             s1Len += 1
+            s1Copy.append(s1[i].lower())
     for i in range(len(s2)):
         if not s2[i].isspace():
             s2Len += 1
+            s2Copy.append(s2[i].lower())
     if s1Len != s2Len:
         return False
+    charChecker = [0] * s1Len
     for i in range(s1Len):
-        if s2[i].lower() not in s1.lower():
+        if s2Copy[i] not in s1Copy:
+            return False
+        for j in range(s2Len):
+            if s2Copy[i] == s1Copy[i]:
+                charChecker[i] += 1
+    for i in range(len(charChecker)):
+        if charChecker[i] % 2 != 0:
             return False
     return True
 
@@ -20,3 +30,5 @@ print(anagram('God', 'Dog'))
 print(anagram('penis','spine'))
 print(anagram('clint eastwood','old west action'))
 print(anagram('python', 'javascript'))
+print(anagram('good', 'god'))
+print(anagram('clint eastwood', 'tld west action'))
