277e972a34937fbccd6b59a70fc9bd5c17d3c080
Carlyle <zachjohnsonsoftware@gmail.com>
2018-08-16 18:05:34 -0500

    anagram fixed

--- arrays/anagram.py
+++ arrays/anagram.py
@@ -3,32 +3,24 @@
 def anagram(s1, s2):
     
     s1Len = s2Len = 0
-    s1Copy = s2Copy = []    
+    s1Copy = []    
+    s2Copy = []
     for i in range(len(s1)):
         if not s1[i].isspace():
             s1Len += 1
             s1Copy.append(s1[i].lower())
-    for i in range(len(s2)):
-        if not s2[i].isspace():
+    for j in range(len(s2)):
+        if not s2[j].isspace():
             s2Len += 1
-            s2Copy.append(s2[i].lower())
-    if s1Len != s2Len:
+            s2Copy.append(s2[j].lower())
+    
+    s1Copy.sort()
+    s2Copy.sort()
+    if s1Copy != s2Copy:
         return False
-    charChecker = [0] * s1Len
-    for i in range(s1Len):
-        if s2Copy[i] not in s1Copy:
-            return False
-        for j in range(s2Len):
-            if s2Copy[i] == s1Copy[i]:
-                charChecker[i] += 1
-    for i in range(len(charChecker)):
-        if charChecker[i] % 2 != 0:
-            return False
     return True
 
 print(anagram('God', 'Dog'))
-print(anagram('penis','spine'))
 print(anagram('clint eastwood','old west action'))
-print(anagram('python', 'javascript'))
 print(anagram('good', 'god'))
 print(anagram('clint eastwood', 'tld west action'))
