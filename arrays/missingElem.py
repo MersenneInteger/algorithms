8932ce6d53b06be73def1e5c16df2b58c1d02d64
Carlyle <zachjohnsonsoftware@gmail.com>
2018-08-28 17:53:15 -0500

    missing elem

--- arrays/missingElem.py
+++ arrays/missingElem.py
@@ -1,3 +1,5 @@
+import collections
+
 #find the missing element
 def finder(arr1, arr2):
 
@@ -18,3 +20,40 @@ def finder(arr1, arr2):
 print(finder([1,2,3,4,5,6,7], [3,7,2,1,4,6]))
 print(finder([5,5,7,7], [5,5,7]))
 print(finder([10,3,3,1,1], [10, 3, 1, 1]))
+
+def finder2(arr1, arr2):
+    
+    arr1.sort()
+    arr2.sort()
+
+    for num1, num2 in zip(arr1, arr2):
+        if num1 != num2:
+            return num1
+    return arr1[-1]
+
+print(finder([1,2,3], [1,3]))
+
+#using hash tables
+def finder3(arr1, arr2):
+
+    d = collections.defaultdict(int)
+    for num in arr2:
+        d[num] += 1
+    for num in arr1:
+        if d[num] == 0:
+            return num
+        else:
+            d[num] -= 1
+
+print(finder3([5,5,7,7], [5,5,7]))
+print('-' * 10)
+
+def finder4(arr1, arr2):
+
+    result = 0
+    for num in arr1 + arr2:
+        result ^= num
+        print(result)
+    print('missing elem: {}'.format(result))
+
+finder4([5,5,7,7], [5,5,7])
